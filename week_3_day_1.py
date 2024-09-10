4ea38b739708e658161cc28513e3bbf9d4563277
Gutierrez <kdgutierrez@cps.k12.il.us>
2024-09-10 14:25:45 -0500

    first commit

---
+++ week_3_day_1.py
@@ -0,0 +1,67 @@
+# # Week3
+# # This week we will work on :
+# # Working With Strings
+
+
+# # 1.   Working With Numbers
+# # 2.   Getting Input From Users
+
+
+
+
+# # 1.   Building a Basic Calculator
+# # 2.   Mad Libs Game
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Review
+# create variables for the following :
+# 1. age
+# 2. name
+# 3. song
+# 4. food
+# 5. number
+
+
+# #now include the variables you just made print in the following...
+
+
+# Once upon a time, there was a [age] old coder named [name].
+
+
+# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+
+
